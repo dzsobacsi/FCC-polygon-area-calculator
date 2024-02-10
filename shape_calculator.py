3122d40c49731960946803e8556579b1f28182ce
dzsobacsi <tberki77@gmail.com>
2024-02-10 16:02:07 +0000

    Solution to FCC polygon area calculator

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,54 @@
 class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
+    def set_width(self, width):
+        self.width = width
 
+    def set_height(self, height):
+        self.height = height
 
+    def get_area(self):
+        return self.width * self.height
 
-class Square:
+    def get_perimeter(self):
+        return 2 * (self.width + self.height)
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** 0.5
+
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture."
+        pic = ""
+        for _ in range(self.height):
+            pic += "*" * self.width + "\n"
+        return pic
+
+    def get_amount_inside(self, shape):
+        return (self.height // shape.height) * (self.width // shape.width)
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(width={self.width}, height={self.height})"
+
+class Square(Rectangle):
+    def __init__(self, side):
+        self.width = side
+        self.height = side
+
+    def set_side(self, side):
+        self.width = side
+        self.height = side
+
+    def set_width(self, width):
+        self.width = width
+        self.height = width
+
+    def set_height(self, height):
+        self.width = height
+        self.height = height
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(side={self.width})"
+    
